28701649a92e75ca112b9b644a65dad40de49399
JO MINGYU <jmg21128@gmail.com>
2022-05-21 14:50:17 +0900

    [Bronze III] Title: A+B - 8, Time: 292 ms, Memory: 19212 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList sum = new ArrayList<>();
+        ArrayList c = new ArrayList<>();
+        ArrayList d = new ArrayList<>();
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            c.add(a);
+            d.add(b);
+            sum.add(a + b);
+        }
+
+        for (int i = 0; i < sum.size(); i++) {
+            System.out.println("Case #" + (i + 1) + ": " + c.get(i) + " + " + d.get(i) + " = " + sum.get(i));
+        }
+    }
+}
