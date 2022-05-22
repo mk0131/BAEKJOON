76988800c7656776c0b609c51d78e4f8ff2d13a5
JO MINGYU <jmg21128@gmail.com>
2022-05-22 22:29:56 +0900

    [Bronze II] Title: 정수 N개의 합, Time: 32 ms, Memory: 422972 KB -BaekjoonHub

---
+++ 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class Test {
+    long sum(int[] a) {
+        long num =0;
+		for(int i = 0; i<a.length; i++) {
+			num +=a[i];
+		}
+		long ans = num;
+		return ans;
+    }
+}
