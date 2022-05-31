89c77903c29c2bdbc9ef009139b6443aeb4c502e
JO MINGYU <jmg21128@gmail.com>
2022-05-31 23:02:27 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 372 ms, Memory: 21420 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		BigInteger a = new BigInteger(sc.next());
+		BigInteger b = new BigInteger(sc.next());
+		
+		a = a.add(b); // a+b;
+		
+		System.out.println(a);
+	}
+}
